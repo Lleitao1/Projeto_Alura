5c47aceaf1510adf05d5c124103fb5e8d5163eab
Lleitao1 <lucas.leitao25@gmail.com>
2020-12-18 18:07:11 -0300

    adicionar classes

--- Projeto_Alura/Controller/ViewController.swift
+++ Projeto_Alura/Controller/ViewController.swift
@@ -6,15 +6,38 @@
 //  Copyright © 2020 Lucas Abdel Leitao. All rights reserved.
 //
 
+//
+//  ViewController.swift
+//  teste
+//
+//  Created by Lucas Abdel Leitao on 16/12/20.
+//  Copyright © 2020 Lucas Abdel Leitao. All rights reserved.
+//
+
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    @IBOutlet var nomeTextField: UITextField?
+    @IBOutlet weak var felicidadeTextField: UITextField?
+    
+    @IBAction func adicionar(_ sender: Any){
+        
+        
+        guard let nomeDaRefeicao = nomeTextField?.text else {
+            return
+        }
+        
+        
+        guard let felicidadeDaRefeicao = felicidadeTextField?.text else {
+            return
+        }
+        
+        let refeicao = Refeicao( nome: nomeDaRefeicao, felicidade: felicidadeDaRefeicao)
+        
+        
+        print(" voce adicionou \(refeicao.nome) de \(refeicao.felicidade)")
     }
-
-
 }
 
--- Projeto_Alura/Model/Item.swift
+++ Projeto_Alura/Model/Item.swift
@@ -9,5 +9,11 @@
 import UIKit
 
 class Item: NSObject {
-
+    var nome: String
+    var calorias: Double
+    
+    init(nome: String, calorias: Double) {
+        self.nome = nome
+        self.calorias = calorias
+    }
 }
