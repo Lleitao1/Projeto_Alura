5cadb387c35f44ee0ecbe7d5ec291d06d67a80b1
Lleitao1 <lucas.leitao25@gmail.com>
2020-12-18 17:59:35 -0300

    first commit

---
+++ Projeto_Alura/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Projeto_Alura
+//
+//  Created by Lucas Abdel Leitao on 18/12/20.
+//  Copyright © 2020 Lucas Abdel Leitao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Projeto_Alura/Model/Refeicao.swift
@@ -0,0 +1,13 @@
+//
+//  Refeicao.swift
+//  Projeto_Alura
+//
+//  Created by Lucas Abdel Leitao on 18/12/20.
+//  Copyright © 2020 Lucas Abdel Leitao. All rights reserved.
+//
+
+import UIKit
+
+class Refeicao: NSObject {
+
+}
